8bf8720237409288d197894ae0c99cf471c228c4
LuanAGoncalves <eng.luangonsalves@gmail.com>
2019-10-03 17:38:39 -0300

    Updating run_service_detection.py

--- run_service_detection.py
+++ run_service_detection.py
@@ -2,20 +2,30 @@ import sys
 
 import cv2
 
+import pandas as pd
+
 from vision.ssd.mobilenet_v2_ssd_lite import (
-    create_mobilenetv2_ssd_lite, create_mobilenetv2_ssd_lite_predictor)
-from vision.ssd.mobilenetv1_ssd import (create_mobilenetv1_ssd,
-                                        create_mobilenetv1_ssd_predictor)
+    create_mobilenetv2_ssd_lite,
+    create_mobilenetv2_ssd_lite_predictor,
+)
+from vision.ssd.mobilenetv1_ssd import (
+    create_mobilenetv1_ssd,
+    create_mobilenetv1_ssd_predictor,
+)
 from vision.ssd.mobilenetv1_ssd_lite import (
-    create_mobilenetv1_ssd_lite, create_mobilenetv1_ssd_lite_predictor)
+    create_mobilenetv1_ssd_lite,
+    create_mobilenetv1_ssd_lite_predictor,
+)
 from vision.ssd.squeezenet_ssd_lite import (
-    create_squeezenet_ssd_lite, create_squeezenet_ssd_lite_predictor)
+    create_squeezenet_ssd_lite,
+    create_squeezenet_ssd_lite_predictor,
+)
 from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
 from vision.utils.misc import Timer
 
 if len(sys.argv) < 4:
     print(
-        'Usage: python run_ssd_example.py <net type>  <model path> <label path> <video file>'
+        "Usage: python run_ssd_example.py <net type>  <model path> <label path> <video file>"
     )
     sys.exit(0)
 net_type = sys.argv[1]
@@ -29,15 +39,15 @@ class_names = [name.strip() for name in open(label_path).readlines()]
 num_classes = len(class_names)
 
 
-if net_type == 'vgg16-ssd':
+if net_type == "vgg16-ssd":
     net = create_vgg_ssd(len(class_names), is_test=True)
-elif net_type == 'mb1-ssd':
+elif net_type == "mb1-ssd":
     net = create_mobilenetv1_ssd(len(class_names), is_test=True)
-elif net_type == 'mb1-ssd-lite':
+elif net_type == "mb1-ssd-lite":
     net = create_mobilenetv1_ssd_lite(len(class_names), is_test=True)
-elif net_type == 'mb2-ssd-lite':
+elif net_type == "mb2-ssd-lite":
     net = create_mobilenetv2_ssd_lite(len(class_names), is_test=True)
-elif net_type == 'sq-ssd-lite':
+elif net_type == "sq-ssd-lite":
     net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
 else:
     print(
@@ -46,15 +56,15 @@ else:
     sys.exit(1)
 net.load(model_path)
 
-if net_type == 'vgg16-ssd':
+if net_type == "vgg16-ssd":
     predictor = create_vgg_ssd_predictor(net, candidate_size=200)
-elif net_type == 'mb1-ssd':
+elif net_type == "mb1-ssd":
     predictor = create_mobilenetv1_ssd_predictor(net, candidate_size=200)
-elif net_type == 'mb1-ssd-lite':
+elif net_type == "mb1-ssd-lite":
     predictor = create_mobilenetv1_ssd_lite_predictor(net, candidate_size=200)
-elif net_type == 'mb2-ssd-lite':
+elif net_type == "mb2-ssd-lite":
     predictor = create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200)
-elif net_type == 'sq-ssd-lite':
+elif net_type == "sq-ssd-lite":
     predictor = create_squeezenet_ssd_lite_predictor(net, candidate_size=200)
 else:
     print(
@@ -62,35 +72,59 @@ else:
     )
     sys.exit(1)
 
-main_wnd_name = 'annotated'
+main_wnd_name = "annotated"
 cv2.namedWindow(main_wnd_name, cv2.WINDOW_NORMAL)
+
+signal = []
+
+c1, c2 = 20, 90
 
 timer = Timer()
 while True:
     ret, orig_image = cap.read()
     if orig_image is None:
+        signal.append(0)
         continue
+    m, n, c = orig_image.shape
+    x, xf, y, yf = c1, m - c1, c2, n - c2
+    orig_image = orig_image[x:xf, y:yf]
     image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
     timer.start()
-    boxes, labels, probs = predictor.predict(image, 10, 0.7)
+    boxes, labels, probs = predictor.predict(image, 10, 0.5)
     interval = timer.end()
     # print('Time: {:.2f}s, Detect Objects: {:d}.'.format(interval, labels.size(0)))
     for i in range(boxes.size(0)):
-        box = boxes[i, :]
-        label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
-        cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
+        if labels[i].numpy() == 15:
+            signal.append(1)
+            box = boxes[i, :]
+            label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
+            cv2.rectangle(
+                orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4
+            )
 
-        cv2.putText(
-            orig_image,
-            label,
-            (box[0] + 20, box[1] + 40),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,  # font scale
-            (255, 0, 255),
-            2,
-        )  # line type
+            cv2.putText(
+                orig_image,
+                label,
+                (box[0] + 20, box[1] + 40),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,  # font scale
+                (255, 0, 255),
+                2,
+            )  # line type
     cv2.imshow(main_wnd_name, orig_image)
-    if cv2.waitKey(int(1 / fps * 1000)) & 0xFF == ord('q'):
+
+    if len(box) == 0:
+        signal.append(0)
+
+    if cv2.waitKey(int(1 / fps * 1000)) & 0xFF == ord("q"):
+        df = pd.DataFrame(signal, columns=["Signal"])
+        df.to_csv(
+            "./" + sys.argv[4].split("/")[-1].split(".")[0] + ".csv",
+            header=None,
+            index=None,
+            sep=" ",
+            mode="a",
+        )
         break
 cap.release()
 cv2.destroyAllWindows()
